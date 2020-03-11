170c9183aeb9f8bcba04372144429be1c3d0726d
noyobo <noyobo@gmail.com>
2020-03-11 23:08:53 +0800

    feat: add d.ts

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare module 'platform.macro' {
+  /**
+   * web 平台
+   */
+  export const isWeb: boolean;
+  /**
+   * 微信小程序
+   */
+  export const isWechat: boolean;
+  /**
+   * 字节跳动小程序
+   */
+  export const isToutiao: boolean;
+}
